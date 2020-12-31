9230a67afb3b44bee165fe7c863abbb8d4d93701
lansel <794069324@qq.com>
2020-12-31 14:43:57 +0800

    第一次测试提交

---
+++ src/main/java/lky/exercise/App.java
@@ -0,0 +1,13 @@
+package lky.exercise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
